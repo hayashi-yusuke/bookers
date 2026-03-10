912a292f1f5c2ab949bf7e7f14608f68b2c49a8c
hayashi-yusuke <262776262+hayashi-yusuke@users.noreply.github.com>
2026-03-10 15:08:15 +0000

    Add basic presence validations for Book model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else 
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -21,15 +26,19 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book)
-    else render :edit
+    else
+      render :edit
     end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
